2812c6bfa8ed6f41eb365ea9e9b9629e7fd45a86
asmaahassoneh <asmaa.hassoneh04@gmail.com>
2026-04-01 16:59:25 +0300

    Auto-create admin user on startup

--- app/__init__.py
+++ app/__init__.py
@@ -76,4 +76,18 @@ def create_app(config_class=Config):
     with app.app_context():
         db.create_all()
 
+        admin_email = "admin@gmail.com"
+        admin_password = "Admin123"
+
+        existing_admin = User.query.filter_by(email=admin_email).first()
+        if not existing_admin:
+            admin = User(
+                username="admin",
+                email=admin_email,
+                is_admin=True,
+            )
+            admin.set_password(admin_password)
+            db.session.add(admin)
+            db.session.commit()
+
     return app
